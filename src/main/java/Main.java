53cb6b343c196b94c42b5dd369b4781bb737234c
OrlovDmitry <orlovdv@inbox.ru>
2023-01-30 17:01:48 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+import reactor.core.publisher.BaseSubscriber;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+
+    }
+}
